f3c47b007fee4b33bbbc67221d72c899c38e6d10
starfall-miao <starfall.miao@gmail.com>
2026-07-31 13:32:33 +0800

    fix(mission-6): 添加 file_picker 导入

--- lib/features/paipan/views/paipan_page.dart
+++ lib/features/paipan/views/paipan_page.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:provider/provider.dart';
 
 import '../../../core/theme/theme_provider.dart';
